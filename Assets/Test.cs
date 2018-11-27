6886c0993af76c0221d2ad642719b7ec07ed8593
水野俊樹 <mofu_neu299@softbank.ne.jp>
2018-11-27 21:32:02 +0900

    課題1
    配列の宣言とBossクラスです。

---
+++ Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Boss {
+
+	// Use this for initialization
+	private int mp = 53;
+	private int mpw = 30;
+	public void Magic () {
+		if (mp >= 5) {
+			this.mp -= 5;
+			Debug.Log (this.mpw + "の魔法攻撃ダメージを与えた。残りMPは" + this.mp + "です。");
+		}
+		else { Debug.Log("MPが足りないため魔法が使えない。");
+		}
+	}
+}
+
+
+public class Test : MonoBehaviour {
+	void Start () {
+		Boss lastboss = new Boss ();
+		lastboss.Magic ();
+	}
+
+
+}
+
+// Update is called once per frame
